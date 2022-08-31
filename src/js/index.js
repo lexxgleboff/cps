0ac4fc1508bb1d1ba14d7e5496530f473fad97b2
Alex Glebov <lexxgleboff@gmail.com>
2022-08-31 11:45:18 +0300

    соеденил все блоки и доделал интерактив

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import '../scss/style.scss';
 import Swiper from './swiper.js'
 
 let init = false;
-let swiper = Swiper;
+let swiperBrands
+let swiperElectronics
+let swiperPrice
 
 function swiperMode() {
     let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
@@ -13,7 +15,7 @@ function swiperMode() {
     if (mobile.matches) {
         if (!init) {
             init = true;
-            swiper = new Swiper('.brands-slider', {
+            swiperBrands = new Swiper('.brands-slider', {
               // spaceBetween: 16,
               slidesPerView: 1.2,
               watchOverflow: true,
@@ -25,14 +27,48 @@ function swiperMode() {
                   0: {
                     slidesPerView: "auto",
                   },
+              },
+              // loop: true,
+          });
+          swiperElectronics = new Swiper('.electronics-slider', {
+              // spaceBetween: 16,
+              slidesPerView: 1.2,
+              watchOverflow: true,
+              pagination: {
+                  el: '.electronics-slider__pagination',
+                  clickable: true,
+              },
+              breakpoints: {
+                  0: {
+                    slidesPerView: "auto",
+                  },
+              },
+              // loop: true,
+          });
+          swiperPrice = new Swiper('.price-slider', {
+              // spaceBetween: 16,
+              slidesPerView: 1.2,
+              watchOverflow: true,
+              pagination: {
+                  el: '.price-slider__pagination',
+                  clickable: true,
+              },
+              breakpoints: {
+                  0: {
+                    slidesPerView: "auto",
+                  },
                 },
           });
         }
     } else if((tablet.matches) && (init)) {
-        swiper.destroy();
+        swiperBrands.destroy();
+        swiperElectronics.destroy();
+        swiperPrice.destroy();
         init = false;
     } else if((desktop.matches) && (init)) {
-        swiper.destroy();
+        swiperBrands.destroy();
+        swiperElectronics.destroy();
+        swiperPrice.destroy();
         init = false;
     }
 }
@@ -48,95 +84,221 @@ window.addEventListener('resize', function() {
 });
 
 
-const clickButton = document.querySelector('.brands__btn');
-const slideShow = document.querySelectorAll('.brands-slider__slide');
+const btnBrands = document.querySelector('.brands__btn');
+const sliderBrands = document.querySelector('.brands-slider');
 
-clickButton.addEventListener('click', function () {
-    if (clickButton.textContent != 'Скрыть') {
-        clickButton.textContent = 'Скрыть';
-    } else {
-        clickButton.textContent = 'Показать всё'
-    }
-    for (let i = 0; i < slideShow.length; i++) {
-        slideShow[i].classList.toggle('brands-slider__slide--show')
-    }
+btnBrands.addEventListener('click', function () {
+  if (btnBrands.textContent != 'Скрыть') {
+    btnBrands.textContent = 'Скрыть';
+  } else {
+    btnBrands.textContent = 'Показать всё'
+  }
+  sliderBrands.classList.toggle('brands-slider--show')
+  btnBrands.classList.toggle('btn-more--rotate')
 })
 
-
-
-const menu = document.querySelector('.menu')
-
-document.addEventListener('click', menuShow)
-
-function menuShow(event) {
-  if (event.target.closest('.header__burger')) {
-    menu.classList.add('menu--active')
-    document.querySelector('.main').classList.add('main--mute')
-    document.querySelector('.header').classList.add('header--mute')
+const btnEl = document.querySelector('.electronics__btn')
+const electronicsSlider = document.querySelector('.electronics-slider')
+btnEl.addEventListener('click', function () {
+  console.log(btnEl);
+  if (btnEl.textContent != 'Скрыть') {
+    btnEl.textContent = 'Скрыть';
+  } else {
+    btnEl.textContent = 'Показать всё'
   }
-  if (event.target.closest('.menu__burger')) {
-    menu.classList.remove('menu--active')
-    document.querySelector('.main').classList.remove('main--mute')
-    document.querySelector('.header').classList.remove('header--mute')
-
-  }
-  if ((!event.target.closest('.menu')) && !(event.target.closest('.header__burger'))) {
-    menu.classList.remove('menu--active')
-    document.querySelector('.main').classList.remove('main--mute')
-    document.querySelector('.header').classList.remove('header--mute')
-  }
-}
-
+  electronicsSlider.classList.toggle('electronics-slider--show')
+  btnEl.classList.toggle('btn-more--rotate')
+})
 
 function muteMenu() {
   let desktop = window.matchMedia('(min-width: 1120px)');
 
   if (desktop.matches) {
-    document.querySelector('.menu').classList.remove('menu--active')
-    document.querySelector('.main').classList.remove('main--mute')
-    document.querySelector('.header').classList.remove('header--mute')
+    menu.classList.remove('menu--active')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
   }
 }
 
-const navigation = document.getElementsByClassName('.navigation__list')
-const navigationItem = document.getElementsByClassName('.navigation__item')
-const active = document.getElementsByClassName('.navigation__item--active')
+const menu = document.getElementById('menu')
+const menuBurger = document.querySelector('.menu__burger')
+const main = document.querySelector('.main')
+const header = document.querySelector('.header')
+const headerMute = document.querySelector('.header--mute')
+const headerBurger = document.querySelector('.header__burger')
+const feedbackChat = document.querySelectorAll('.menu-feedback__chat')
+const feedbackCall = document.querySelectorAll('.menu-feedback__call')
+const feedbackClose = document.querySelector('.feedback__close')
+const feedback = document.getElementById('feedback')
+const call = document.getElementById('call')
+const callClose = document.querySelector('.call__close')
+const navigation = document.querySelectorAll('.navigation__item')
 
 for (let i = 0; i < navigation.length; i++) {
-  navigation[i].onClick = () => {
-
-    if (active[0])
-      currentActive.classList.remove("navigation__item--active");
-
-    if (active[0] !== this)
-      this.classList.add("navigation__item--active");
-  }
+  navigation[i].addEventListener('click', addClass)
 }
 
-// for (let i = 0; navigation.length > i; i++) {
-//   navigation[i].addEventListener('click', () => {
-//     if (active[i]) {
+function addClass() {
+  const active = document.getElementsByClassName('navigation__item--active')
+  active[0].className = active[0].className.replace(' navigation__item--active', '')
+  this.className += ' navigation__item--active'
+}
 
-//     }
-//   })
-// }
 
-// for (let item of navigation) {
-//   navigation.addEventListener('click', () => {
-//     if (item.contains('.navigation__item--active')) {
-//       item.classList.remove('.navigation__item--active')
-//     }
-//     if (!item.contains('.navigation__item--active')) {
-//       item.classList.add('.navigation__item--active')
-//     }
-//   })
-// }
+headerBurger.addEventListener('click', (event) => {
+  if (event.target.closest('.header__burger')) {
+    menu.classList.add('menu--active')
+    main.classList.add('main--mute')
+    header.classList.add('header--mute')
+  }
+})
 
-// menu.addEventListener('click', () => {
-//   for (let item of navigation) {
-//     console.log(item);
-//     item.classList.add('navigation__item--active')
+menuBurger.addEventListener('click', (event) => {
+  if (event.target.closest('.menu__burger')) {
+    menu.classList.remove('menu--active')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+  }
+})
+document.addEventListener('click', (event) => {
+  if ((event.target.closest('.main')) && (!event.target.closest('.menu-feedback__chat')) && (!event.target.closest('.header__burger'))) {
+    menu.classList.remove('menu--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+    feedback.classList.remove('feedback--active')
+    call.classList.remove('call--active')
+
+  }
+
+  if ((event.target.closest('.header')) && (!event.target.closest('.menu-feedback__chat')) && (!event.target.closest('.menu-feedback__call')) && (!event.target.closest('.header__burger'))) {
+    call.classList.remove('call--active')
+    feedback.classList.remove('feedback--active')
+    menu.classList.remove('menu--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+  }
+  if ((event.target.closest('.menu')) && (!event.target.closest('.menu-feedback__chat')) && (!event.target.closest('.menu-feedback__call'))) {
+    call.classList.remove('call--active')
+    feedback.classList.remove('feedback--active')
+    menu.classList.remove('menu--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+  }
+
+   if ((feedback.classList.contains('feedback--active')) && (event.target)) {
+    menu.classList.remove('menu--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    call.classList.remove('call--active')
+
+  }
+   if ((call.classList.contains('call--active')) && (event.target)) {
+    menu.classList.remove('menu--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    feedback.classList.remove('feedback--active')
+
+  }
+   if (feedback.classList.contains('feedback--active')) {
+    menu.classList.remove('menu--active')
+    menu.classList.add('menu--mute')
+    main.classList.add('main--mute')
+    header.classList.add('header--mute')
+    call.classList.remove('call--active')
+
+  }
+   if (call.classList.contains('call--active')) {
+    menu.classList.remove('menu--active')
+    menu.classList.add('menu--mute')
+    main.classList.add('main--mute')
+    header.classList.add('header--mute')
+    feedback.classList.remove('feedback--active')
+
+  }
+
+
+})
+
+
+
+// function menuShow(event) {
+
+//   if (event.target.closest('.menu__burger')) {
+//     menu.classList.remove('menu--active')
+//     main.classList.remove('main--mute')
+//     header.classList.remove('header--mute')
 
 //   }
+  // if ((!event.target.closest('.menu')) && (!event.target.closest('.header__burger')) && (!event.target.closest('.menu-feedback__chat'))) {
+  //   menu.classList.remove('menu--active')
+  //   menu.classList.remove('menu--mute')
+  //   main.classList.remove('main--mute')
+  //   header.classList.remove('header--mute')
+  //   feedback.classList.remove('feedback--active')
+  // }
+  // if (((!event.target.closest('.menu-feedback__chat')) && (!event.target.closest('.feedback')))) {
+  //   menu.classList.remove('menu--active')
+  //   menu.classList.remove('menu--mute')
+  //   main.classList.remove('main--mute')
+  //   header.classList.remove('header--mute')
+  //   feedback.classList.remove('feedback--active')
+  // }
 
-// })
+  // if ((feedback.classList.contains('feedback--active')) && (event.target)) {
+  //   menu.classList.remove('menu--active')
+  //   menu.classList.remove('menu--mute')
+
+  // }
+
+
+  // if (event.target.closest('.main--mute') && (event.target.closest('.menu--mute'))) {
+  //   feedback.classList.remove('feedback--active')
+  //   menu.classList.remove('menu--mute')
+  //   main.classList.remove('main--mute')
+  // }
+// }
+
+
+
+
+
+for (let i = 0; i < feedbackCall.length; i++) {
+  feedbackCall[i].addEventListener('click', (event) => {
+    event.preventDefault()
+    if (!event.target.closest('.call')) {
+      call.classList.toggle('call--active')
+    }
+  })
+  feedbackChat[i].addEventListener('click', (event) => {
+    event.preventDefault()
+    if (!event.target.closest('.feedback')) {
+      feedback.classList.toggle('feedback--active')
+    }
+  })
+}
+
+feedbackClose.addEventListener('click', (event) => {
+  event.preventDefault()
+
+    feedback.classList.remove('feedback--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+
+})
+
+
+
+callClose.addEventListener('click', (event) => {
+  event.preventDefault()
+
+    call.classList.remove('call--active')
+    menu.classList.remove('menu--mute')
+    main.classList.remove('main--mute')
+    header.classList.remove('header--mute')
+
+})
+
